d8b621528143b4d7a90ed5f74fc00cd8a6c57e38
Occult09 <maheermusfiq221@gmail.com>
2026-05-03 20:16:36 +0600

    generic in typescript

---
+++ src/generic.ts
@@ -0,0 +1,30 @@
+// dynamically generalized
+type generic<value> = Array<value>
+
+
+// const friends: string[] = ['a', 'b', 'c'];
+const friends: generic<string> = ['a', 'b', 'c'];
+
+// const rollNumbers: number[] = [4, 5, 6];
+const rollNumbers: generic<number> = [4, 5, 6];
+
+// const isElligibleList: boolean[] = [true, false, true];
+const isElligibleList: generic<boolean> = [true, false, true];
+
+// tuples
+type Coordinates<x, y> = [x, y];
+
+const coordinates1 = [20, 30];
+const coordinates2 = ['20', '30'];
+
+// object
+const userList: generic<{ name: string; age: number; }> = [
+    {
+        name: 'x',
+        age: 22
+    },
+    {
+        name: 'y',
+        age: 23
+    }
+]
