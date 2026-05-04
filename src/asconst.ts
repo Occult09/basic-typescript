f9c3ef05b99c0c20ec04f3bbcbf3e385917d6773
Occult09 <maheermusfiq221@gmail.com>
2026-05-04 19:32:47 +0600

    asConst

---
+++ src/asconst.ts
@@ -0,0 +1,17 @@
+// as const assertion
+
+const UserRoles = {
+    Admin: 'Admin',
+    Editor: 'Editor',
+    Viewer: 'Viewer'
+} as const;
+
+const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
+    if (role === UserRoles.Admin || role === UserRoles.Editor) {
+        return true;
+    }
+    else return false;
+};
+
+const isEditPermissible = canEdit(UserRoles.Admin);
+console.log(isEditPermissible);
